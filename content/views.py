770e8c7308702c48446fa0e58d490795d111b4be
SeokHyun Bae <bjh4966@naver.com>
2023-10-11 15:43:28 +0900

    1st

---
+++ content/views.py
@@ -0,0 +1,9 @@
+
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') #select * from content_feed;
+        return render(request, 'mungstargram/main.html',context=dict(feeds=feed_list))
