d338fc384326f485b1fa6a0d81ebebdffadcadbe
SeokHyun Bae <bjh4966@naver.com>
2023-10-14 15:54:49 +0900

    2nd

--- mungstargram/urls.py
+++ mungstargram/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from .view import Sub
-from content.views import Main
+from content.views import Main, UploadFeed
+from .settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main',Main.as_view())
+    path('main', Main.as_view()),
+    path('content/upload', UploadFeed.as_view())
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
